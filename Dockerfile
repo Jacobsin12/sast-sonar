1ce5d7732283436f7deeaadaf2da6012e0617d58
Jacobsin <tristanjacob755@gmail.com>
2026-03-15 18:26:51 -0600

    Codigo bien

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,9 @@ FROM node:18
 WORKDIR /app
 COPY package*.json ./
 RUN npm install
-COPY . .
+# Cambiamos el dueño de los archivos al usuario node
+COPY --chown=node:node . .  
+# Usamos el usuario no-root
+USER node
 EXPOSE 3000
 CMD ["node", "index.js"]
--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// 1. SEGURIDAD: Deshabilitar la cabecera que revela Express (Fix SonarQube)
+app.disable('x-powered-by'); 
+
+// 2. RUTAS
 app.get('/', (req, res) => {
   res.send('¡Servidor Node.js funcionando y desplegadoooo!');
 });
 
+// 3. INICIO DEL SERVIDOR
 app.listen(port, '0.0.0.0', () => {
-  console.log(`App escuchando en el puertoo ${port}`);
+  console.log(`App escuchando en el puerto ${port}`);
 });
