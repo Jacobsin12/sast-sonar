a7cd2089d2ca122cd13e8301b86247406af925fa
Jacobsin <tristanjacob755@gmail.com>
2026-03-19 19:38:25 -0600

    Grafana y prometheus

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
 const express = require('express');
+const client = require('prom-client'); // <--- NUEVO: Cliente de Prometheus
 const app = express();
 const port = 3000;
 
-// 1. SEGURIDAD: Deshabilitar la cabecera que revela Express (Fix SonarQube)
-app.disable('x-powered-by'); 
+app.disable('x-powered-by');
 
-// 2. RUTAS
-app.get('/', (req, res) => {
-  res.send('¡Servidor Node.js funcionando y desplegadoooo!');
+// --- CONFIGURACIÓN DE MÉTRICAS ---
+client.collectDefaultMetrics(); // Métricas de RAM/CPU del contenedor
+
+const httpRequestCounter = new client.Counter({
+  name: 'http_requests_total',
+  help: 'Total de peticiones HTTP procesadas',
+  labelNames: ['metodo', 'ruta', 'estado_http'],
 });
 
-// 3. INICIO DEL SERVIDOR
+// --- MIDDLEWARE PARA CONTAR PETICIONES ---
+app.use((req, res, next) => {
+  res.on('finish', () => {
+    httpRequestCounter.labels(req.method, req.path, res.statusCode).inc();
+  });
+  next();
+});
+
+// --- RUTA PARA PROMETHEUS ---
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', client.register.contentType);
+  res.send(await client.register.metrics());
+});
+
+app.get('/', (req, res) => {
+  res.send('¡Servidor funcionando y siendo monitoreado! 🚀');
+});
+
 app.listen(port, '0.0.0.0', () => {
   console.log(`App escuchando en el puerto ${port}`);
 });
